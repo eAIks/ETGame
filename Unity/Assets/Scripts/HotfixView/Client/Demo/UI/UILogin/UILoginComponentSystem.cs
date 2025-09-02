3dcbaf36f55edbcf8b239c8ee6ab916d80854d39
wanghongwei <qq728708439@gmail.com>
2025-09-02 18:52:14 +0800

    update 添加修改UILogin 添加注册

--- Unity/Assets/Scripts/HotfixView/Client/Demo/UI/UILogin/UILoginComponentSystem.cs
+++ Unity/Assets/Scripts/HotfixView/Client/Demo/UI/UILogin/UILoginComponentSystem.cs
@@ -16,6 +16,20 @@ namespace ET.Client
 			self.loginBtn.GetComponent<Button>().onClick.AddListener(()=> { self.OnLogin(); });
 			self.account = rc.Get<GameObject>("Account");
 			self.password = rc.Get<GameObject>("Password");
+			self.registerBtn = rc.Get<GameObject>("RegisterBtn");
+			self.registerBtn.GetComponent<Button>().onClick.AddListener(()=> { self.OnShowRegisterPanel(); });
+
+			self.reAccount = rc.Get<GameObject>("ReAccount");
+			self.rePassword = rc.Get<GameObject>("RePassword");
+			self.reRegistrBtn = rc.Get<GameObject>("ReRegisterBtn");
+			self.loginBtn.GetComponent<Button>().onClick.AddListener(()=> { self.OnRegister(); });
+
+			self.returnBtn = rc.Get<GameObject>("ReturnBtn");
+			self.returnBtn.GetComponent<Button>().onClick.AddListener(()=> { self.OnShowLoginPanel(); });
+
+			self.loginPanel = rc.Get<GameObject>("LoginPanel");
+			self.registerPanel = rc.Get<GameObject>("RegisterPanel");
+
 		}
 
 		
@@ -25,6 +39,28 @@ namespace ET.Client
 				self.Root(), 
 				self.account.GetComponent<InputField>().text, 
 				self.password.GetComponent<InputField>().text).Coroutine();
+		}
+
+		public static void OnRegister(this UILoginComponent self)
+		{
+			LoginHelper.Login(
+				self.Root(), 
+				self.reAccount.GetComponent<InputField>().text, 
+				self.rePassword.GetComponent<InputField>().text).Coroutine();
+		}
+		
+		public static void OnShowRegisterPanel(this UILoginComponent self)
+		{
+			Log.Error("显示注册页面");
+			self.loginPanel.SetActive(false);
+			self.registerPanel.SetActive(true);
+		}
+
+		public static void OnShowLoginPanel(this UILoginComponent self)
+		{
+			Log.Error("显示登录页面");
+			self.loginPanel.SetActive(true);
+			self.registerPanel.SetActive(false);
 		}
 	}
 }
--- Unity/Assets/Scripts/ModelView/Client/Demo/UI/UILogin/UILoginComponent.cs
+++ Unity/Assets/Scripts/ModelView/Client/Demo/UI/UILogin/UILoginComponent.cs
@@ -8,5 +8,12 @@ namespace ET.Client
 		public GameObject account;
 		public GameObject password;
 		public GameObject loginBtn;
+		public GameObject registerBtn;
+		public GameObject reAccount;
+		public GameObject rePassword;
+		public GameObject reRegistrBtn;
+		public GameObject returnBtn;
+		public GameObject loginPanel;
+		public GameObject registerPanel;
 	}
 }
